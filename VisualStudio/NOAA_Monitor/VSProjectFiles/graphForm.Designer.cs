b1f262e609b03dc85427f1938729c56abf40faf4
Dylan Rogers <11dylan.rogers@gmail.com>
2018-12-01 20:51:43 -0500

    Added graphing functionality to the graphing window. Along with data selection

--- VisualStudio/NOAA_Monitor/VSProjectFiles/graphForm.Designer.cs
+++ VisualStudio/NOAA_Monitor/VSProjectFiles/graphForm.Designer.cs
@@ -28,20 +28,96 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            this.chart1.BackColor = System.Drawing.Color.RoyalBlue;
+            this.chart1.BackSecondaryColor = System.Drawing.Color.White;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.BackColor = System.Drawing.Color.RoyalBlue;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(0, 64);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series1.Legend = "Legend1";
+            series1.MarkerColor = System.Drawing.Color.Black;
+            series1.MarkerSize = 9;
+            series1.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Circle;
+            series1.Name = "DataValues";
+            series1.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Double;
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(1268, 624);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            title1.Name = "GraphTitle";
+            this.chart1.Titles.Add(title1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1126, 222);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Load Graph";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(1102, 177);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.Text = "Make a selection...";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.RoyalBlue;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(1092, 273);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(164, 141);
+            this.textBox1.TabIndex = 4;
             // 
             // graphForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1268, 687);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "graphForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "graphForm";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
--- VisualStudio/NOAA_Monitor/VSProjectFiles/graphForm.cs
+++ VisualStudio/NOAA_Monitor/VSProjectFiles/graphForm.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -14,17 +14,271 @@ namespace NOAA_Monitor
 {
     public partial class graphForm : MaterialForm
     {
-        BuoyList data;
+        BuoyList Stations;
+        Buoy tmpBuoy = new Buoy();
+
+        string maineBuoys =
+            "PSBM1 CFWM1 44027 ATGM1 44034" +
+            "MDRM1 44037 44033 MISM1 44032" +
+            "44005 CASM1 44007 44030 WEQM1 WEXM1 WELM1";
+
         public graphForm(BuoyList tmp)
         {
-            data = tmp;
+            Stations = new BuoyList(tmp);
             InitializeComponent();
+
             comboBox1.Items.Add("Wind Speed");
+            comboBox1.Items.Add("Wind Direction");
+            comboBox1.Items.Add("Dominant Wave");
+            comboBox1.Items.Add("Average Wave Time");
+            comboBox1.Items.Add("Sea Pressure");
+            comboBox1.Items.Add("Air Temperature");
+            comboBox1.Items.Add("Sea Temperature");
+            comboBox1.Items.Add("Dew Temperature");
+            comboBox1.Items.Add("Visibility");
+            comboBox1.Items.Add("Tide Height");
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            chart1.Series[0].Points.Clear();
 
+            List<Buoy>.Enumerator data = Stations.blist.GetEnumerator();
+            int stepper = 0;
+            int graphSelection;
+
+            graphSelection = comboBox1.SelectedIndex;
+
+            switch (graphSelection)
+            {
+                //Wind Speed Graph
+                case 0:
+                    while (data.MoveNext())
+                    {
+                        tmpBuoy = data.Current;
+                        if (maineBuoys.Contains(tmpBuoy.getbname()) && !String.IsNullOrEmpty(tmpBuoy.getbname()))
+                        {
+                            chart1.Series[0].Points.AddY(tmpBuoy.getwindspeed());
+                            chart1.Series[0].Points[stepper].AxisLabel = tmpBuoy.getbname();
+                            chart1.Series[0].Points[stepper].IsValueShownAsLabel = true;
+                            stepper++;
+                        }
+                    }
+                    windSpeedText();
+                    break;
+
+                //Wind Direction Graph
+                case 1:
+                    while (data.MoveNext())
+                    {
+                        tmpBuoy = data.Current;
+                        if (maineBuoys.Contains(tmpBuoy.getbname()) && !String.IsNullOrEmpty(tmpBuoy.getbname()))
+                        {
+                            chart1.Series[0].Points.AddY(tmpBuoy.getwinddirection());
+                            chart1.Series[0].Points[stepper].AxisLabel = tmpBuoy.getbname();
+                            chart1.Series[0].Points[stepper].IsValueShownAsLabel = true;
+                            stepper++;
+                        }
+                    }
+                    windDirectionText();
+                    break;
+
+                //Dominant Wave Period Graph
+                case 2:
+                    while (data.MoveNext())
+                    {
+                        tmpBuoy = data.Current;
+                        if (maineBuoys.Contains(tmpBuoy.getbname()) && !String.IsNullOrEmpty(tmpBuoy.getbname()))
+                        {
+                            chart1.Series[0].Points.AddY(tmpBuoy.getdomwave());
+                            chart1.Series[0].Points[stepper].AxisLabel = tmpBuoy.getbname();
+                            chart1.Series[0].Points[stepper].IsValueShownAsLabel = true;
+                            stepper++;
+                        }
+                    }
+                    dominantWavePeriodText();
+                    break;
+
+                //Average Wave Period Graph
+                case 3:
+                    while (data.MoveNext())
+                    {
+                        tmpBuoy = data.Current;
+                        if (maineBuoys.Contains(tmpBuoy.getbname()) && !String.IsNullOrEmpty(tmpBuoy.getbname()))
+                        {
+                            chart1.Series[0].Points.AddY(tmpBuoy.getavewave());
+                            chart1.Series[0].Points[stepper].AxisLabel = tmpBuoy.getbname();
+                            chart1.Series[0].Points[stepper].IsValueShownAsLabel = true;
+                            stepper++;
+                        }
+                    }
+                    averageWavePeriodText();
+                    break;
+
+                //Sea Pressure Graph
+                case 4:
+                    while (data.MoveNext())
+                    {
+                        tmpBuoy = data.Current;
+                        if (maineBuoys.Contains(tmpBuoy.getbname()) && !String.IsNullOrEmpty(tmpBuoy.getbname()))
+                        {
+                            chart1.Series[0].Points.AddY(tmpBuoy.getseapress());
+                            chart1.Series[0].Points[stepper].AxisLabel = tmpBuoy.getbname();
+                            chart1.Series[0].Points[stepper].IsValueShownAsLabel = true;
+                            stepper++;
+                        }
+                    }
+                    seaPressureText();
+                    break;
+
+                //Air Tempreature Graph
+                case 5:
+                    while (data.MoveNext())
+                    {
+                        tmpBuoy = data.Current;
+                        if (maineBuoys.Contains(tmpBuoy.getbname()) && !String.IsNullOrEmpty(tmpBuoy.getbname()))
+                        {
+                            chart1.Series[0].Points.AddY(tmpBuoy.getairtemp());
+                            chart1.Series[0].Points[stepper].AxisLabel = tmpBuoy.getbname();
+                            chart1.Series[0].Points[stepper].IsValueShownAsLabel = true;
+                            stepper++;
+                        }
+                    }
+                    airTemperatureText();
+                    break;
+
+                //Sea Temperature Graph
+                case 6:
+                    while (data.MoveNext())
+                    {
+                        tmpBuoy = data.Current;
+                        if (maineBuoys.Contains(tmpBuoy.getbname()) && !String.IsNullOrEmpty(tmpBuoy.getbname()))
+                        {
+                            chart1.Series[0].Points.AddY(tmpBuoy.getseatemp());
+                            chart1.Series[0].Points[stepper].AxisLabel = tmpBuoy.getbname();
+                            chart1.Series[0].Points[stepper].IsValueShownAsLabel = true;
+                            stepper++;
+                        }
+                    }
+                    seaTemperatureText();
+                    break;
+
+                //Dew Temperature Graph
+                case 7:
+                    while (data.MoveNext())
+                    {
+                        tmpBuoy = data.Current;
+                        if (maineBuoys.Contains(tmpBuoy.getbname()) && !String.IsNullOrEmpty(tmpBuoy.getbname()))
+                        {
+                            chart1.Series[0].Points.AddY(tmpBuoy.getdewtemp());
+                            chart1.Series[0].Points[stepper].AxisLabel = tmpBuoy.getbname();
+                            chart1.Series[0].Points[stepper].IsValueShownAsLabel = true;
+                            stepper++;
+                        }
+                    }
+                    dewTemperatureText();
+                    break;
+
+                //Visibility Distance Graph
+                case 8:
+                    while (data.MoveNext())
+                    {
+                        tmpBuoy = data.Current;
+                        if (maineBuoys.Contains(tmpBuoy.getbname()) && !String.IsNullOrEmpty(tmpBuoy.getbname()))
+                        {
+                            chart1.Series[0].Points.AddY(tmpBuoy.getvis());
+                            chart1.Series[0].Points[stepper].AxisLabel = tmpBuoy.getbname();
+                            chart1.Series[0].Points[stepper].IsValueShownAsLabel = true;
+                            stepper++;
+                        }
+                    }
+                    visibilityText();
+                    break;
+
+                //Tide Height Graph
+                case 9:
+                    while (data.MoveNext())
+                    {
+                        tmpBuoy = data.Current;
+                        if (maineBuoys.Contains(tmpBuoy.getbname()) && !String.IsNullOrEmpty(tmpBuoy.getbname()))
+                        {
+                            chart1.Series[0].Points.AddY(tmpBuoy.gettide());
+                            chart1.Series[0].Points[stepper].AxisLabel = tmpBuoy.getbname();
+                            chart1.Series[0].Points[stepper].IsValueShownAsLabel = true;
+                            stepper++;
+                        }
+                    }
+                    tideHeightText();
+                    break;
+
+                default:
+                    break;
+            }
+
+            //Displays the series value to the chart area
+            chart1.Series[0].ChartArea = "ChartArea1";
+
+            //Displays each values buoy name as a corresponding x-axis value
+            chart1.ChartAreas[0].AxisX.Interval = 1;
+
+            //Explination for the 0 values in the chart
+            textBox1.AppendText(Environment.NewLine + Environment.NewLine 
+                +"*For all 0 values in the graph assume the buoy or land station did not take a reading");
+        }
+
+        public void windSpeedText()
+        {
+            textBox1.Text = "This graph displays the wind speed recorded from the buoy sensors in meter per second. "
+                + "These measurements were recorded over an average period of 8 minutes for buoys and 2 minutes for land stations.";
+        }
+
+        public void windDirectionText()
+        {
+            textBox1.Text = "This graph displays the wind direction in degrees (°) clockwise from true North "
+                + "during the same period used for the wind speed.";
+        }
+
+        public void dominantWavePeriodText()
+        {
+            textBox1.Text = "This graph displays the dominant wave period in seconds. "
+                + "This time is fro the period with the maximum wave energy.";
+        }
+
+        public void averageWavePeriodText()
+        {
+            textBox1.Text = "This graph displays the average wave period in seconds. "
+                + "This was recorded for all waves in a 20-minute period";
+        }
+
+        public void seaPressureText()
+        {
+            textBox1.Text = "This graph displays the sea pressure in hPa.";
+        }
+
+        public void airTemperatureText()
+        {
+            textBox1.Text = "This graph displays the air temperature in °C";
+        }
+
+        public void seaTemperatureText()
+        {
+            textBox1.Text = "This graph displays the sea temperature in °C";
+        }
+
+        public void dewTemperatureText()
+        {
+            textBox1.Text = "This graph displays the dewpoint temperature in °C. "
+               + "This was taken at the same height as the air temperature";
+        }
+
+        public void visibilityText()
+        {
+            textBox1.Text = "This graph displays the visibility distance in nautical miles";
+        }
+
+        public void tideHeightText()
+        {
+            textBox1.Text = "This graph displays the water level in feet above of below Mean Low Water";
         }
     }
 }
