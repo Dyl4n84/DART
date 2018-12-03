75ad53840506875520590f2a6fd6c4c59448c565
JMbcat212 <jm139216@ohio.edu>
2018-12-03 17:40:26 -0500

    Added some more doxygen comments

--- VisualStudio/NOAA_Monitor/VSProjectFiles/buoylist.cs
+++ VisualStudio/NOAA_Monitor/VSProjectFiles/buoylist.cs
@@ -5,20 +5,19 @@ using System.IO;
 
 public class BuoyList
 {
-    /**
-     *
-     *
-     *
+    List<Buoy> blist; ///< The list of buoy classes
+    /** The constructor of the class
      */
-    public List<Buoy> blist;
-	public BuoyList()
+    public BuoyList()
 	{
         blist = new List<Buoy>();
 	}
-
+    /** Loads from the text file into a list of buoys
+     * @param The filename of the input file, preferably a text file
+     */
     public void load(string file)
     {
-        string[] lines = File.ReadAllLines("C:\\Users\\11dyl\\Documents\\School\\Fall18\\TestingDir\\VisualStudio\\NOAA_Monitor\\VSProjectFiles\\bin\\Debug\\latest_obs-11_30_2018_22_54.txt");
+        string[] lines = File.ReadAllLines(file);
         for (int i = 2; i < lines.Length; i++) //i starts at two to skip the first two header lines of the file.
         {
             Buoy temp = new Buoy();
@@ -26,12 +25,16 @@ public class BuoyList
             blist.Add(temp);
         }
     }
-
+    /** Displays the information about a buoy
+     * @param The index of the buoy list to be displayed
+     */
     public void bdisplay(int index)
     {
         blist[index].display();
     }
-
+    /** Searches through the list for a specific buoy
+     * @param The ID (banme) of the buoy to be searched
+     */
     public void search(string target)
     {
         for(int i = 0; i < blist.Count; i++){
@@ -44,7 +47,9 @@ public class BuoyList
         Console.WriteLine("Not found");
         return;
     }
-
+    /** Copy constructor
+    * @param buoy list to be copied from
+    */
     public BuoyList(BuoyList a)
     {
         blist = new List<Buoy>();
--- VisualStudio/buoyclassVS/buoyclassVS/buoylist.cs
+++ VisualStudio/buoyclassVS/buoyclassVS/buoylist.cs
@@ -3,35 +3,38 @@ using System.Collections.Generic;
 using System.IO;
 
 
-public class buoylist
+public class BuoyList
 {
-    /**
-     * 
-     * 
-     * 
+    List<Buoy> blist; ///< The list of buoy classes
+    /** The constructor of the class
      */
-    List<buoy> blist;
-	public buoylist()
+    public BuoyList()
 	{
-        blist = new List<buoy>();
+        blist = new List<Buoy>();
 	}
-
+    /** Loads from the text file into a list of buoys
+     * @param The filename of the input file, preferably a text file
+     */
     public void load(string file)
     {
         string[] lines = File.ReadAllLines(file);
         for (int i = 2; i < lines.Length; i++) //i starts at two to skip the first two header lines of the file.
         {
-            buoy temp = new buoy();
+            Buoy temp = new Buoy();
             temp.input(lines[i]);
             blist.Add(temp);
         }
     }
-
+    /** Displays the information about a buoy
+     * @param The index of the buoy list to be displayed
+     */
     public void bdisplay(int index)
     {
         blist[index].display();
     }
-
+    /** Searches through the list for a specific buoy
+     * @param The ID (banme) of the buoy to be searched
+     */
     public void search(string target)
     {
         for(int i = 0; i < blist.Count; i++){
@@ -44,10 +47,12 @@ public class buoylist
         Console.WriteLine("Not found");
         return;
     }
-
-    public buoylist(buoylist a)
+    /** Copy constructor
+    * @param buoy list to be copied from
+    */
+    public BuoyList(BuoyList a)
     {
-        blist = new List<buoy>();
+        blist = new List<Buoy>();
         int size = a.blist.Count;
         for (int i=0; i < size; i++)
         {
